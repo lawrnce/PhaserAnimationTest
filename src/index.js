038774776e65f66fdde402890fbc49acd559f7ab
Lawrence Tran <lawrence.c.tran@outlook.com>
2018-04-23 13:03:25 -0700

    Add test sprite sheet

--- src/index.js
+++ src/index.js
@@ -1,34 +1,35 @@
 import 'phaser';
 
 var config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create
-    }
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 360,
+  height: 520,
+  scene: {
+    preload: preload,
+    create: create
+  }
 };
 
 var game = new Phaser.Game(config);
 
+var character;
+
 function preload ()
 {
-    this.load.image('logo', 'assets/logo.png');
+  this.load.atlas('test_sheet', 'assets/test_sheet.png', 'assets/test_sheet.json');
 }
 
 function create ()
 {
-    var logo = this.add.image(400, 150, 'logo');
+  character = this.add.sprite(150, 400, 'test_sheet','SimplePeople_Pimp_Black_[Front<0,5,8>]_[Party_Dance_01_000].png' );
+  character.setScale(0.5, 0.5);
 
-    this.tweens.add({
-        targets: logo,
-        y: 450,
-        duration: 2000,
-        ease: 'Power2',
-        yoyo: true,
-        loop: -1
-    });
+  var frameNames = this.anims.generateFrameNames('test_sheet', {
+    start: 0, end: 26, zeroPad: 3,
+    prefix: 'SimplePeople_Pimp_Black_[Front<0,5,8>]_[Party_Dance_01_', suffix: '].png'
+  });
 
+  this.anims.create({ key: 'party_dance', frames: frameNames, frameRate: 24, repeat: -1 });
+  character.anims.play('party_dance');
 }
